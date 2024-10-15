56e62c7682599b3873a690833f1242cba98f4520
Kirill Korepanov <xorboo@gmail.com>
2024-10-15 03:25:40 +0200

    Fix discord template aspect ratio

--- static/generator/js/generator-meetup.js
+++ static/generator/js/generator-meetup.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  window.IMAGE_WIDTHS = ["2400px", "3600px"];
+  window.IMAGE_WIDTHS = ["2400px", "3375px"];
   window.IMAGE_TYPE = "meetup";
   initializeAndGenerate("meetup-template.html");
   setListeners();
